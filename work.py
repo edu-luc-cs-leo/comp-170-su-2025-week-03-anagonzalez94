e31bf05b35ab1c173ade180cd80f0de7b73b4a73
Ana Gonzalez <157073381+anagonzalez94@users.noreply.github.com>
2025-06-18 15:19:43 +0000

    WIP

--- work.py
+++ work.py
@@ -1,8 +1,20 @@
 # Code
 
 ## Diamond Shape
+def draw_diamond(height: int):
+    # to calculate the number of hashes and spaces, we need the function 2x+1
+    for i in range(height):
+        print(" " * (height - i), "#" * (2*i+1))
+    for i in range(height -2, -1, -1):
+        print(" " * (height - i), "#" * (2*i+1))
 
 ## Right Triangle
+def draw_triangle(height: int):
 
 ## Compound Interest
+def compound_interest(principal: int, interest: int, time: int):
+    rate = interest / 100
+    for i in range(time):
 
+
+## Hollow Square
